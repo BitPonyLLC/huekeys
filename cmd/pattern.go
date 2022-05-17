4dfa7b143196bc39088581a6eabc43dc26a6d0d0
bradrf <brad@bitpony.com>
2022-05-17 07:58:24 -0700

    be nice by default

--- cmd/pattern.go
+++ cmd/pattern.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"syscall"
 	"time"
 
 	keyboard "github.com/bambash/sys76-kb/pkg"
@@ -15,11 +16,23 @@ var Pattern string
 // Delay represents the amount of time to wait between updates
 var Delay time.Duration
 
+// NiceLevel represents the priority of the process
+var NiceLevel int
+
 func init() {
 	rootCmd.AddCommand(runCmd)
 	runCmd.Flags().StringVarP(&Pattern, "pattern", "p", "", "the pattern to run: rainbow, pulse, random")
 	runCmd.Flags().DurationVarP(&Delay, "delay", "d", 0,
 		"the amount of time to wait between updates (units: ns, us, ms, s, m, h)")
+	runCmd.Flags().IntVar(&NiceLevel, "nice", 10, "the priority level of the process")
+}
+
+func beNice() {
+	pid := syscall.Getpid()
+	err := syscall.Setpriority(syscall.PRIO_PROCESS, pid, NiceLevel)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to set nice level %d: %v\n", NiceLevel, err)
+	}
 }
 
 var runCmd = &cobra.Command{
@@ -28,6 +41,7 @@ var runCmd = &cobra.Command{
 	Long:  `runs a pattern that the backlight loops through. 'rainbow' or 'pulse'`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if Pattern != "" {
+			beNice()
 			fmt.Printf("running pattern %v\n", Pattern)
 			switch Pattern {
 			case "rainbow":
