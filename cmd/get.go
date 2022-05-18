80a24eba2fe66d8dc8a009b65d190ebb51831f6b
bradrf <brad@bitpony.com>
2022-05-18 11:23:14 -0700

    add note about opting to set group perm

--- cmd/get.go
+++ cmd/get.go
@@ -18,7 +18,7 @@ var getCmd = &cobra.Command{
 	Short: "Gets the color and brightness of the keyboard",
 	Long:  `Gets the color and brightness of the keyboard`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("brightness =", keyboard.GetCurrentBrightness(), "more")
+		fmt.Println("brightness =", keyboard.GetCurrentBrightness())
 		for key, color := range keyboard.GetCurrentColors() {
 			fmt.Printf("%s = %s\n", key, color)
 		}
