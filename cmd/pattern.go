c8b5c417291b68b726c0d325e75793cb42b4b36b
bradrf <brad@bitpony.com>
2022-05-20 14:19:39 -0700

    more refactor of pattern cmds

--- cmd/pattern.go
+++ cmd/pattern.go
@@ -12,30 +12,46 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// Pattern represents keyboard color pattern to run
-var Pattern string
-
-// Delay represents the amount of time to wait between updates
-var Delay time.Duration
-
-// NiceLevel represents the priority of the process
-var NiceLevel int
-
 func init() {
-	rootCmd.AddCommand(runCmd)
-	runCmd.Flags().IntVar(&NiceLevel, "nice", 10, "the priority level of the process")
+	pidpath := "/tmp/sys76-kb.pid"
+	priority := 10
 
+	runCmd := &cobra.Command{
+		Use:   "run",
+		Short: "runs a backlight pattern",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			err := checkAndSetPidPath(pidpath)
+			if err != nil {
+				cmd.PrintErr(err)
+				os.Exit(1)
+			}
+
+			err = beNice(priority)
+			if err != nil {
+				cmd.PrintErr(err)
+				os.Exit(2)
+			}
+		},
+		PersistentPostRun: func(cmd *cobra.Command, args []string) {
+			os.Remove(pidpath)
+		},
+	}
+	rootCmd.AddCommand(runCmd)
+	runCmd.Flags().StringVar(&pidpath, "pidpath", pidpath, "pathname of the pidfile")
+	runCmd.Flags().IntVar(&priority, "nice", 10, "the priority level of the process")
+
+	var delay time.Duration
 	var patternCmd *cobra.Command
 
 	addDelayFlag := func(value time.Duration) {
-		patternCmd.Flags().DurationVarP(&Delay, "delay", "d", value,
+		patternCmd.Flags().DurationVarP(&delay, "delay", "d", value,
 			"the amount of time to wait between updates (units: ns, us, ms, s, m, h)")
 	}
 
 	patternCmd = &cobra.Command{
 		Use:   "pulse",
 		Short: "pulse the keyboard brightness up and down",
-		Run:   func(_ *cobra.Command, _ []string) { keyboard.BrightnessPulse(Delay) },
+		Run:   func(_ *cobra.Command, _ []string) { keyboard.BrightnessPulse(delay) },
 	}
 	addDelayFlag(25 * time.Millisecond)
 	runCmd.AddCommand(patternCmd)
@@ -43,7 +59,7 @@ func init() {
 	patternCmd = &cobra.Command{
 		Use:   "rainbow",
 		Short: "loop through all the colors of the rainbow",
-		Run:   func(_ *cobra.Command, _ []string) { keyboard.InfiniteRainbow(Delay) },
+		Run:   func(_ *cobra.Command, _ []string) { keyboard.InfiniteRainbow(delay) },
 	}
 	addDelayFlag(time.Nanosecond)
 	runCmd.AddCommand(patternCmd)
@@ -51,7 +67,7 @@ func init() {
 	patternCmd = &cobra.Command{
 		Use:   "random",
 		Short: "constantly change the color to a random selection",
-		Run:   func(_ *cobra.Command, _ []string) { keyboard.InfiniteRandom(Delay) },
+		Run:   func(_ *cobra.Command, _ []string) { keyboard.InfiniteRandom(delay) },
 	}
 	addDelayFlag(1 * time.Second)
 	runCmd.AddCommand(patternCmd)
@@ -59,7 +75,7 @@ func init() {
 	patternCmd = &cobra.Command{
 		Use:   "cpu",
 		Short: "change the color according to CPU utilization (cold to hot)",
-		Run:   func(_ *cobra.Command, _ []string) { keyboard.MonitorCPU(Delay) },
+		Run:   func(_ *cobra.Command, _ []string) { keyboard.MonitorCPU(delay) },
 	}
 	addDelayFlag(1 * time.Second)
 	runCmd.AddCommand(patternCmd)
@@ -68,11 +84,10 @@ func init() {
 	patternCmd = &cobra.Command{
 		Use:   "typing",
 		Short: "change the color according to typing speed (cold to hot)",
-		Run:   func(_ *cobra.Command, _ []string) { keyboard.MonitorTyping(Delay, inputEventID) },
+		Run:   func(_ *cobra.Command, _ []string) { keyboard.MonitorTyping(delay, inputEventID) },
 	}
 	addDelayFlag(300 * time.Millisecond)
-	patternCmd.Flags().StringVarP(&inputEventID, "input-event-id", "i", inputEventID,
-		"input event ID to monitor")
+	patternCmd.Flags().StringVarP(&inputEventID, "input-event-id", "i", inputEventID, "input event ID to monitor")
 	runCmd.AddCommand(patternCmd)
 
 	patternCmd = &cobra.Command{
@@ -83,51 +98,28 @@ func init() {
 	runCmd.AddCommand(patternCmd)
 }
 
-var runCmd = &cobra.Command{
-	Use:   "run",
-	Short: "runs a backlight pattern",
-	Long:  `runs a backlight pattern`,
-	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-		err := checkAndSetPidPath()
-		if err != nil {
-			return err
-		}
-
-		err = beNice()
-		if err != nil {
-			return err
-		}
-
-		fmt.Printf("running pattern %v\n", Pattern)
-		return nil
-	},
-	PersistentPostRun: func(cmd *cobra.Command, args []string) {
-		os.Remove(pidpath)
-	},
-}
-
-const pidpath = "/tmp/sys76-kb.pid"
-
-func checkAndSetPidPath() error {
+func checkAndSetPidPath(pidpath string) error {
 	otherPidContent, err := os.ReadFile(pidpath)
 	if err == nil {
 		var otherPid int
 		otherPid, err = strconv.Atoi(string(otherPidContent))
 		if err != nil {
-			panic(err)
+			return fmt.Errorf("unable to parse contents of %s: %w", pidpath, err)
 		}
 
 		err = syscall.Kill(otherPid, 0)
-		if err == nil {
+		if err == nil || err.(syscall.Errno) == syscall.EPERM {
+			// if EPERM, process is owned by another user, probably root
 			return fmt.Errorf("process %d is already running a pattern", otherPid)
 		}
 
+		// ESRCH: no such process
 		if err.(syscall.Errno) != syscall.ESRCH {
-			return err
+			return fmt.Errorf("unable to check if process %d is still running: %w", otherPid, err)
 		}
 	} else {
 		if !os.IsNotExist(err) {
-			return err
+			return fmt.Errorf("unable to read %s: %w", pidpath, err)
 		}
 	}
 	err = os.WriteFile(pidpath, []byte(fmt.Sprint(os.Getpid())), 0666)
@@ -147,11 +139,11 @@ func checkAndSetPidPath() error {
 	return nil
 }
 
-func beNice() error {
+func beNice(priority int) error {
 	pid := syscall.Getpid()
-	err := syscall.Setpriority(syscall.PRIO_PROCESS, pid, NiceLevel)
+	err := syscall.Setpriority(syscall.PRIO_PROCESS, pid, priority)
 	if err != nil {
-		return fmt.Errorf("unable to set nice level %d: %w", NiceLevel, err)
+		return fmt.Errorf("unable to set nice level %d: %w", priority, err)
 	}
 	return nil
 }
